2524685a85c09d1e4c21bca20ea0ad343dfe4de8
EasyElric <fgoyer3856@gmail.com>
2016-03-28 11:08:40 -0500

    Added Owner Home Activity

---
+++ app/src/main/java/com/mobile4623/easy/adogption/OwnerHome.java
@@ -0,0 +1,13 @@
+package com.mobile4623.easy.adogption;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OwnerHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_home);
+    }
+}
