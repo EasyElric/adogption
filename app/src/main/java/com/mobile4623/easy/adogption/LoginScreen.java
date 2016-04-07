064c233be73f5f3addf350a7e6cf1daf12f0c758
EasyElric <fgoyer3856@gmail.com>
2016-04-06 21:19:16 -0500

    Button and Facebook
    Added a button drawable with rounded corners, and started the Facebook
    Login SDK process

--- app/src/main/java/com/mobile4623/easy/adogption/LoginScreen.java
+++ app/src/main/java/com/mobile4623/easy/adogption/LoginScreen.java
@@ -1,16 +1,20 @@
 package com.mobile4623.easy.adogption;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+import com.facebook.FacebookSdk;
 
 public class LoginScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login_screen);
+
     }
 
 
