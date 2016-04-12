29064c6ef790829606534cda5abe49aa9648f081
EasyElric <fgoyer3856@gmail.com>
2016-04-12 12:03:50 -0500

    New Icon
    Added new launcher icon

--- app/src/main/java/com/mobile4623/easy/adogption/OwnerHome.java
+++ app/src/main/java/com/mobile4623/easy/adogption/OwnerHome.java
@@ -1,7 +1,7 @@
 package com.mobile4623.easy.adogption;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class OwnerHome extends AppCompatActivity {
 
--- app/src/main/java/com/mobile4623/easy/adogption/UserHome.java
+++ app/src/main/java/com/mobile4623/easy/adogption/UserHome.java
@@ -1,11 +1,7 @@
 package com.mobile4623.easy.adogption;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class UserHome extends AppCompatActivity {
 
