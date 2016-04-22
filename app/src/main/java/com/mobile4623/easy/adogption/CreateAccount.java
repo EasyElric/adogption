2749ca9b406f0bacde5a66291c75090a966fa2a5
EasyElric <fgoyer3856@gmail.com>
2016-04-21 20:30:22 -0500

    Create Account Activity

--- app/src/main/java/com/mobile4623/easy/adogption/CreateAccount.java
+++ app/src/main/java/com/mobile4623/easy/adogption/CreateAccount.java
@@ -1,13 +1,38 @@
 package com.mobile4623.easy.adogption;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class CreateAccount extends AppCompatActivity {
+
+    Button btnAdd;
+    EditText txtName;
+    EditText txtPass;
+
+    private ProgressDialog pDialog;
+
+    // JSON parser class
+    JSONParser jsonParser = new JSONParser();
+
+    // JSON Node names
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+
+        Spinner animalType = (Spinner) findViewById(R.id.account_type);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.account_type_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        animalType.setAdapter(adapter);
     }
 }
